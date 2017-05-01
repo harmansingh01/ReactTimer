b96fa2f9bd0667caec43f8047afebff32570d5c0
harman.singh01 <harman.singh@etax.com.au>
2017-05-01 16:23:30 +1000

    First push

--- public/components/main.js
+++ public/components/main.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import Nav from "./Nav";
 
 const Main = (props) => {
   return <div>
+    <Nav/>
     <div className="row">
       <div>React Boilerplate</div>
       <div className="columns medium-6 large-4 small-centered"> {props.children}</div>
