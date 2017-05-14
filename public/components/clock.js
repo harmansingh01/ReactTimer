183765a3483a889d7a681d4a7fc7b7585e4b380a
harman.singh01 <harman.singh@etax.com.au>
2017-05-14 15:08:42 +1000

    added first tests

--- public/components/clock.js
+++ public/components/clock.js
@@ -1,43 +1,50 @@
-
 import React, {Component} from 'react';
 
-export default  class Clock extends Component{
+export default  class Clock extends Component {
 
+  formatSeconds(totalSeconds) {
+    let seconds = totalSeconds % 60;
+    let minutes = Math.floor(totalSeconds / 60);
 
-  formatSeconds(totalSeconds){
-    let seconds=totalSeconds%60;
-    let minutes= Math.floor(totalSeconds/60);
-
-    if(seconds<10){
-      seconds='0'+seconds;
+    if (seconds < 10) {
+      seconds = '0' + seconds;
     }
-    if(minutes<10){
-      minutes='0'+minutes
+    if (minutes < 10) {
+      minutes = '0' + minutes
     }
     return `${minutes}:${seconds}`;
   }
-  render(){
+
+  render() {
     const clockStyle = {
-      color: 'blue'
-
+      alignItems: 'center',
+      backgroundColor: '#B5D0E2',
+      border: '2px solid #2099E8',
+      borderRadius: '50%',
+      display: 'flex',
+      height:'14rem',
+      justifyContent:'center',
+      margin:'4rem auto',
+      width:'14rem'
     };
-    const clock_text={
-
+    const clock_text = {
+      color: '#ffffff',
+      fontSize: '2.25rem'
     };
 
-    let {totalSeconds}=this.props;
-    return(
-    <div style={clockStyle}>
-      <span style={clock_text}>{this.formatSeconds(totalSeconds)}</span>
-    </div>
+    let {totalSeconds} = this.props;
+    return (
+      <div style={clockStyle}>
+        <span style={clock_text}>{this.formatSeconds(totalSeconds)}</span>
+      </div>
     );
   }
 }
 
-Clock.defaultProps={
-  totalSeconds:0
+Clock.defaultProps = {
+  totalSeconds: 0
 };
 
-Clock.propTypes={
-  totalSeconds:React.PropTypes.number
+Clock.propTypes = {
+  totalSeconds: React.PropTypes.number
 };
